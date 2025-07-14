32db80be974a8a62fd39f695377a6d75773cf87d
PedroLesbrao <lifesandroid4@gmail.com>
2025-07-14 00:14:56 -0300

    git push

--- api/homepage.php
+++ api/homepage.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial</title>
     <link rel="stylesheet" href="/css/style.css">
-        <script src="/js/inicial.js"></script>
+        <script src="/js/inicial.js" defer></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
